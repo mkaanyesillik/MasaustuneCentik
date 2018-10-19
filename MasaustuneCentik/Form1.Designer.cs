9a472f5b712cf431f49e0fb8ea88786150497014
Kaan Yeşillik <mkaanyesillik@users.noreply.github.com>
2018-10-19 16:55:18 +0300

    Masaüstüne Çentik
    Çentik malum günümüzün popüler donanımı oldu.
    Laptopumda saati sürekli görebileceğim haber başlıklarını takip edebileceğim bir uygulama yapayım dedim ve ortaya bu çıktı.

---
+++ MasaustuneCentik/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace MasaustuneCentik
+{
+    partial class frmCentik
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCentik));
+            this.lblSaat = new System.Windows.Forms.Label();
+            this.tmrSaat = new System.Windows.Forms.Timer(this.components);
+            this.tmrIndir = new System.Windows.Forms.Timer(this.components);
+            this.tmrKaldir = new System.Windows.Forms.Timer(this.components);
+            this.lblHaber = new System.Windows.Forms.Label();
+            this.tmrHaber = new System.Windows.Forms.Timer(this.components);
+            this.pnlInternet = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // lblSaat
+            // 
+            this.lblSaat.BackColor = System.Drawing.Color.Transparent;
+            this.lblSaat.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblSaat.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaat.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblSaat.Location = new System.Drawing.Point(0, 51);
+            this.lblSaat.Name = "lblSaat";
+            this.lblSaat.Size = new System.Drawing.Size(360, 39);
+            this.lblSaat.TabIndex = 0;
+            this.lblSaat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblSaat.MouseHover += new System.EventHandler(this.lblSaat_MouseHover);
+            // 
+            // tmrSaat
+            // 
+            this.tmrSaat.Interval = 1000;
+            this.tmrSaat.Tick += new System.EventHandler(this.tmrSaat_Tick);
+            // 
+            // tmrIndir
+            // 
+            this.tmrIndir.Interval = 1;
+            this.tmrIndir.Tick += new System.EventHandler(this.tmrIndir_Tick);
+            // 
+            // tmrKaldir
+            // 
+            this.tmrKaldir.Interval = 1;
+            this.tmrKaldir.Tick += new System.EventHandler(this.tmrKaldir_Tick);
+            // 
+            // lblHaber
+            // 
+            this.lblHaber.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblHaber.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblHaber.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHaber.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblHaber.Location = new System.Drawing.Point(0, 0);
+            this.lblHaber.Name = "lblHaber";
+            this.lblHaber.Size = new System.Drawing.Size(360, 51);
+            this.lblHaber.TabIndex = 1;
+            this.lblHaber.MouseLeave += new System.EventHandler(this.lblHaber_MouseLeave);
+            // 
+            // tmrHaber
+            // 
+            this.tmrHaber.Interval = 400;
+            this.tmrHaber.Tick += new System.EventHandler(this.tmrHaber_Tick);
+            // 
+            // pnlInternet
+            // 
+            this.pnlInternet.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pnlInternet.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlInternet.Location = new System.Drawing.Point(315, 63);
+            this.pnlInternet.Name = "pnlInternet";
+            this.pnlInternet.Size = new System.Drawing.Size(15, 15);
+            this.pnlInternet.TabIndex = 2;
+            // 
+            // frmCentik
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(360, 90);
+            this.Controls.Add(this.pnlInternet);
+            this.Controls.Add(this.lblHaber);
+            this.Controls.Add(this.lblSaat);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmCentik";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Çentik";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.AppWorkspace;
+            this.Load += new System.EventHandler(this.frmCentik_Load);
+            this.MouseLeave += new System.EventHandler(this.frmCentik_MouseLeave);
+            this.MouseHover += new System.EventHandler(this.frmCentik_MouseHover);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSaat;
+        private System.Windows.Forms.Timer tmrSaat;
+        private System.Windows.Forms.Timer tmrIndir;
+        private System.Windows.Forms.Timer tmrKaldir;
+        private System.Windows.Forms.Label lblHaber;
+        private System.Windows.Forms.Timer tmrHaber;
+        private System.Windows.Forms.Panel pnlInternet;
+    }
+}
+
---
+++ MasaustuneCentik/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace MasaustuneCentik
+{
+    public partial class frmCentik : Form
+    {
+        public frmCentik()
+        {
+            InitializeComponent();
+        }
+        int indir = 60;
+        int kaldir = 0;
+        int CalistiMi = 0;
+        int ekranortala = Screen.PrimaryScreen.Bounds.Width / 2 - 180;
+        private void frmCentik_Load(object sender, EventArgs e)
+        {
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2 - 180, Screen.PrimaryScreen.Bounds.Height - Screen.PrimaryScreen.Bounds.Height - indir);
+            tmrSaat.Start();
+
+           
+        }
+
+        private void tmrSaat_Tick(object sender, EventArgs e)
+        {
+            lblSaat.Text = DateTime.Now.Hour.ToString("00") + ":" + DateTime.Now.Minute.ToString("00") + ":" + DateTime.Now.Second.ToString("00");
+            if (InternetKontrol() == true)
+            {
+                pnlInternet.BackgroundImage = Image.FromFile(@"intvar.png");
+                
+            }
+            else
+            {
+                pnlInternet.BackgroundImage = Image.FromFile(@"intyok.png");
+            }
+        }
+
+        private void frmCentik_MouseHover(object sender, EventArgs e)
+        {
+            tmrIndir.Start();
+            CalistiMi = 0;
+        }
+
+        private void lblSaat_MouseHover(object sender, EventArgs e)
+        {
+            tmrIndir.Start();
+            HaberlerSlider();
+        }
+
+        private void frmCentik_MouseLeave(object sender, EventArgs e)
+        {
+            tmrKaldir.Start();
+            CalistiMi = 10;
+        }
+
+        private void tmrIndir_Tick(object sender, EventArgs e)
+        {
+            if (CalistiMi == 0)
+            {
+                if (indir == 60 || indir != 0)
+                {
+                    indir--;
+                    ekranortala = Screen.PrimaryScreen.Bounds.Width / 2 - 180;
+                }
+                else
+                {
+                    Baricikart();
+                    tmrIndir.Stop();
+                }
+                this.Location = new Point(ekranortala, Screen.PrimaryScreen.Bounds.Height - Screen.PrimaryScreen.Bounds.Height - indir);
+            }
+            else
+            {
+                CalistiMi = 0;
+                indir = 60;
+                tmrIndir.Start();
+            }
+        }
+
+        private void tmrKaldir_Tick(object sender, EventArgs e)
+        {
+            kaldir = 60;
+            this.Width = 360;
+
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2 - 180, Screen.PrimaryScreen.Bounds.Height - Screen.PrimaryScreen.Bounds.Height - kaldir);
+            tmrKaldir.Stop();
+        }
+
+        private void lblHaber_MouseLeave(object sender, EventArgs e)
+        {
+            tmrKaldir.Start();
+            CalistiMi = 10;
+        }
+
+        private void Baricikart()
+        {
+            this.Width = Screen.PrimaryScreen.Bounds.Width;
+            ekranortala = 0;
+            this.Location = new Point(0);
+        }
+
+        private void HaberlerSlider()
+        {
+            Uri urlhaber = new Uri("https://www.trthaber.com/sitene-ekle/gundem-1/?haberSay=30");
+            WebClient clienthaber = new WebClient();
+
+            string htmlhaber = clienthaber.DownloadString(urlhaber);
+            HtmlAgilityPack.HtmlDocument dokumanhaber = new HtmlAgilityPack.HtmlDocument();
+            dokumanhaber.LoadHtml(htmlhaber);
+            tmrHaber.Enabled = true;
+            
+
+            for (int i = 1; i < 30; i++)
+            {
+                string haberler = dokumanhaber.DocumentNode.SelectNodes("/html/body/div/a[" + i.ToString() + "]/span[1]")[0].InnerText.Replace("quot;", "");
+                byte[] bytes = Encoding.Default.GetBytes(haberler);
+                string falan = Encoding.UTF8.GetString(bytes);
+                lblHaber.Text += falan + " // ";
+
+            }
+        }
+
+        private void tmrHaber_Tick(object sender, EventArgs e)
+        {
+            lblHaber.Text = lblHaber.Text.Substring(1) + lblHaber.Text.Substring(0, 1);
+
+            lblHaber.Text = lblHaber.Text.Substring(1) + lblHaber.Text.Substring(0, 1);
+        }
+        public bool InternetKontrol()
+        {
+            try
+            {
+                System.Net.Sockets.TcpClient kontrol_client = new System.Net.Sockets.TcpClient("www.google.com.tr", 80);
+                kontrol_client.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                
+                return false;
+            }
+        }
+    }
+}
